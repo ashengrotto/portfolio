34833da11596e9b83dfdaadd9d9ec88e3d80898a
ashengrotto <viviana_wei@brown.edu>
2024-05-03 20:12:42 -0400

    add photo carousel

---
+++ script.js
@@ -0,0 +1,20 @@
+const aboutPic = document.getElementById('about-pic');
+let hoverCount = 0;
+
+aboutPic.addEventListener('mouseenter', function() {
+  hoverCount++;
+  switch(hoverCount) {
+    case 1:
+      this.src = "./assets/about-pic-2.JPEG";
+      break;
+    case 2:
+      this.src = "./assets/about-pic-3.JPEG";
+      break;
+    case 3:
+      this.src = "./assets/about-pic-1.JPEG";
+      hoverCount = 0;
+      break;
+    default:
+      break;
+  }
+});
